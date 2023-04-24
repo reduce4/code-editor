bd62f5530f1671e522581499103f7f6cbc4219f6
happysmile <812024380@qq.com>
2023-04-24 12:25:01 +0800

    add

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.module.css";
+
+function App(props) {
+  return <>hello</>;
+}
+
+export default App;
